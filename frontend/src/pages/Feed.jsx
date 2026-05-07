6ade2dfa5a82ec149291218995a1495265c80e04
Matheus Camerini <matheuscameriniberto@gmail.com>
2026-05-07 11:04:32 -0300

    Alcatraz 2.0

--- frontend/src/pages/Feed.jsx
+++
@@ -1,9 +0,0 @@
-import NavBar from "../components/NavBar";
-export default function Feed(){
-    return (
-        <>
-        <NavBar />
-        <h1>Feed</h1>
-        </>
-    )
-}
--- frontend/src/pages/Register.jsx
+++
@@ -1,9 +0,0 @@
-import NavBar from '../components/NavBar'
-export default function Register() {
-    return (
-        <>
-        <NavBar itens={[[{classes: 'li',ids: 'login',href: '/login',text: 'Login'}],[{classes: 'li',ids: '',text: 'Projeto'}]]}/>
-        <h1>Register</h1>
-        </>
-    )
-}
